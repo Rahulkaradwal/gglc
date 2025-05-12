6fffd2f81d20e06360c8a7f21213cddeb8332373
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-05-12 14:52:38 -0400

    fix : paddings in service - hero section

--- components/services/hero.tsx
+++ components/services/hero.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 function Hero() {
   return (
-    <section className="flex mt-2 sm:mt-10   lg:pt-44  lg:flex-row items-center justify-between gap-10 sm:px-6 md:px-16  mb-10">
+    <section className="flex mt-2 sm:mt-10   lg:pt-44  lg:flex-row items-center justify-between gap-1 md:gap-10  sm:px-6 md:px-16  mb-10">
       {/* Text content */}
       <div className="w-2/3 md:text-center md:w-2/5  lg:text-left">
         <h3 className="text-sm md:text-2xl font-bold text-green-700 mb-2">
@@ -20,14 +20,14 @@ function Hero() {
           that scales on demand.
         </p>
         <div>
-          <Button className="text-green-700 border-2 mt-2 h-10 font-semibold md:mt-4 border-green-700 hover:text-white cursor-pointer bg-white">
+          <Button className="text-green-700 border-2  md:h-10 font-semibold mt-4 border-green-700 hover:text-white cursor-pointer bg-white">
             Let&apos;s Talk
           </Button>
         </div>
       </div>
 
       {/* Image */}
-      <div className=" sm:flex-1/2 block relative h-[230px]  w-[300px] md:h-[300px]">
+      <div className=" flex-1/2 block relative h-[230px]  w-[300px] md:h-[300px]">
         <Image
           className="rounded-2xl    object-cover"
           src="/hero-bg.png"
