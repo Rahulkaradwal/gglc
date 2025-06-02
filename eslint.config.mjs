cfc19bd286327e8b6133c484f9f8744df012969e
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-06-01 22:39:40 -0400

    fix: prisma error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["lib/generated/**"], // ✅ Ignore generated files
+    // ignores: ["lib/generated/**"], // ✅ Ignore generated files
   },
 ];
 
